d017b9d08f71fa18548881493a1d4fae6a79698d
sahil kumar <sahilkumar06804@gmail.com>
2026-04-03 21:57:38 +0530

    add LinkedList

---
+++ Collection_Interface/Linkedlist.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Linkedlist {
+    public static void main(String[] args) {
+        List<Integer> lk = new LinkedList<>();
+        lk.add(1);
+        lk.add(2);
+        lk.add(3);
+        lk.add(4);
+        lk.add(5);
+
+        ListIterator<Integer> iterator = lk.listIterator();
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
+        System.out.println(iterator.previous());
+    }
+    
+}
